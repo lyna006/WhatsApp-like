110cc304d7550bd0c93c5e9e064a399f4234b786
Melina <pauporras@estudiantec.cr>
2024-07-27 16:33:47 -0600

    First version Client

--- WhatsApp-like/Client/Program.cs
+++ WhatsApp-like/Client/Program.cs
@@ -1,2 +1,58 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ChatClient
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            if (args.Length < 2 || !int.TryParse(args[1], out int port))
+            {
+                Console.WriteLine("Usage: dotnet run <server-ip> <port>");
+                return;
+            }
+
+            var clientSocket = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+            var endPoint = new IPEndPoint(IPAddress.Parse(args[0]), port);
+
+            try
+            {
+                clientSocket.Connect(endPoint);
+                Console.WriteLine("Connected to server.");
+
+                Task.Run(() => ReceiveMessages(clientSocket));
+
+                while (true)
+                {
+                    var message = Console.ReadLine();
+                    var buffer = Encoding.UTF8.GetBytes(message);
+                    clientSocket.Send(buffer);
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"Error: {ex.Message}");
+            }
+            finally
+            {
+                clientSocket.Close();
+            }
+        }
+
+        private static void ReceiveMessages(Socket clientSocket)
+        {
+            var buffer = new byte[1024];
+            int bytesRead;
+
+            while ((bytesRead = clientSocket.Receive(buffer)) > 0)
+            {
+                var message = Encoding.UTF8.GetString(buffer, 0, bytesRead);
+                Console.WriteLine($"Received: {message}");
+            }
+        }
+    }
+}
